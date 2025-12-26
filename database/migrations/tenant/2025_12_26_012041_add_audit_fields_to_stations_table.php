a8bc94fe951f6ede0867feb5eb3f182d63e4a62e
sylladev1805 <ousmanespatec@gmail.com>
2025-12-26 01:25:00 +0000

    mises a jour

---
+++ database/migrations/tenant/2025_12_26_012041_add_audit_fields_to_stations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('stations', function (Blueprint $table) {
+
+            // Audit fields (ta manière)
+            $table->foreignId('created_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+
+            $table->foreignId('modify_by')
+                ->nullable()
+                ->constrained('users')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('stations', function (Blueprint $table) {
+
+            $table->dropConstrainedForeignId('created_by');
+            $table->dropConstrainedForeignId('modify_by');
+        });
+    }
+};
