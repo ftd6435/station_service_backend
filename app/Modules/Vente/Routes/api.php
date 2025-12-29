0aa7030408f5da9a8e44cb208637a81561b3be78
sylladev1805 <ousmanespatec@gmail.com>
2025-12-29 14:25:46 +0000

    mises a jour

--- app/Modules/Vente/Routes/api.php
+++ app/Modules/Vente/Routes/api.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['station.db','auth:sanctum'])->prefix('v1/vente')->group(function () {
     Route::apiResource('cuves',ProduitController::class);
     //  Route::apiResource('ligne-ventes',LigneVenteController::class);
-    Route::put(
+    Route::post(
     'ligne-ventes/index-fin/{id}',
     [LigneVenteController::class, 'update']
 );
